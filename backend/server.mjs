c7af9ac351b5d738a88dc4c7f6408bdfa9585ba0
SHIKHER09 <SHIKHARRAO1@GMAIL.com>
2023-08-21 03:01:30 +0530

    jai shree ganesh

---
+++ backend/server.mjs
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/db.mjs";
+const app = express();
+dotenv.config();
+connectDB();
+
+
+app.get("/", (req, res) => {
+  res.send("Hello, Express!");
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
