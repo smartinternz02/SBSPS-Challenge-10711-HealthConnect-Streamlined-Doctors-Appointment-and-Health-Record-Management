1528ef77c79aa2feca6f404aaebcd4bbb87775f0
ABHISHEKyadav281 <yyadavabhishek9@gmail.com>
2023-08-29 17:58:41 +0530

    assisment,pshyco comp

--- understanding/src/Screens/Assessment.jsx
+++ understanding/src/Screens/Assessment.jsx
@@ -2,99 +2,66 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import img1 from "../Assets/Psychology1.jpg";
-import '../styles/Assessment.css'; 
+
+import '../styles/Assessment.css';
+import { useNavigate } from 'react-router-dom';
+import Footer from '../Component/Footer';
 
 const AssessmentCarousel = () => {
-  const [responses, setResponses] = useState({});
-  
-  const questions = [
-    {
-      text: 'How often do you feel stressed?',
-      options: ['Never', 'Rarely', 'Sometimes', 'Often', 'Always']
-    },
-    {
-      text: 'Do you experience trouble sleeping?',
-      options: ['No', 'Yes']
-    },
-    {
-      text: 'Are you satisfied with your daily routine?',
-      options: ['Yes', 'No']
-    }
-  ]; 
-  const totalQuestions = questions.length;
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const navigate =useNavigate();
 
-  const handleResponse = (question, answer) => {
-    setResponses({ ...responses, [question]: answer });
+  const [userInput, setUserInput] = useState('');
+
+  const handleUserInput = (e) => {
+    setUserInput(e.target.value);
   };
 
-  const handleNext = () => {
-    const currentQuestion = questions[currentIndex];
-    const currentResponse = responses[currentQuestion.text];
+  const handleForm = async (e) => {
+    e.preventDefault();
 
-    if (!currentResponse) {
-      alert('Please select an option for the current question.');
-      return;
+    try {
+
+      // await axios.post('http://127.0.0.1:5000/chat', {
+      //   user_input: userInput
+      // });
+      navigate("/psychologist-finder")
+
+    } catch (error) {
+      console.error('Error submitting assessment:', error);
     }
-
-    setCurrentIndex(currentIndex + 1);
-  };
-
-  const handleSubmit = () => {
-    // Submit the collected responses to the server
-    
-    console.log(responses);
-    
-    // Send responses to the server
-      
   };
 
   return (
     <>
     <div className="cont">
     <div className="assessment-container">
+
       <div className="image-section">
         <img
           src={img1}
           alt="Doctor's Office"
         />
       </div>
-       <div className="carousel-section">
-      <h2 className="assessment-heading">Mental Health Assessment</h2>
-      <Carousel
-        showThumbs={false}
-        showArrows={true}
-        selectedItem={currentIndex}
-      >
-        {questions.map((question, index) => (
-          <div key={index} className="carousel-slide">
-            <h3 >{question.text}</h3>
-            <select
-              className="response-input"
-              onChange={(e) => handleResponse(question.text, e.target.value)}
-              value={responses[question.text] || ''}
-            >
-              <option value="">Select an option</option>
-              {question.options.map((option, index) => (
-                <option key={index} value={option}>
-                  {option}
-                </option>
-              ))}
-            </select>
-            {index < totalQuestions - 1 && (
-              <button className="next-button" onClick={handleNext}>
-                Next
-              </button>
-            )}
-            {index === totalQuestions - 1 && (
-              <button className="submit-button" onClick={handleSubmit}>
-                Submit
-              </button>
-            )}
-          </div>
-        ))}
-      </Carousel>
+
+      <div className="carousel-section">
+        <h2 className="assessment-heading">Mental Health Talk</h2>
+        <form onSubmit={handleForm}>
+          <h3>Write what you are feeling currently/your query</h3>
+          <textarea
+            className="response-input"
+            onChange={handleUserInput}
+            value={userInput}
+          />
+          <button type="submit" className="submit-button">
+            Submit
+          </button>
+        </form>
+
+
       </div>
+
+
+    </div>
     </div>
     <Footer></Footer>
     </>
